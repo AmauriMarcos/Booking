fd9ac176a8271ff6d6604815a667c87732d7a12b
AmauriMarcos <amaurisantos.m@gmail.com>
2022-07-15 17:30:25 +0200

    start navbar

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/global.css';
+import './styles/fonts/Segoe/SegoeUI.ttf';
+import './styles/fonts/Segoe/SegoeUI-Bold.ttf';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from '../../components/navbar/Navbar';
+import '../../sass/pages/_home.scss';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <Navbar/>
+      <p>Home</p>
+    </div>
   )
 }
 
