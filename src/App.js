1d9e6307f6d6336b0e11b49b46fcd4d6060b3edc
AmauriMarcos <amaurisantos.m@gmail.com>
2022-07-15 12:06:39 +0200

    create scss folder structure and add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/home/Home';
+import List from './pages/list/List';
+import Hotel from './pages/hotel/Hotel';
 
 function App() {
   return (
-    <div className="app">
-        <h1>Booking</h1>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home/>} />
+            <Route  path="/hotels" element={<List/>}/>
+            <Route path="/hotels/:id" element={<Hotel/>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
