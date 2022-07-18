5714dbbeb2e66cd042fc369e235549fd8988684a
AmauriMarcos <amaurisantos.m@gmail.com>
2022-07-18 17:19:11 +0200

    finish hero component

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../sass/sections/_hero.scss';
+
+const Hero = () => {
+  return (
+    <div className="hero">
+        <div className="heroContainer">
+            <h1 className='title'>Find your next stay</h1>
+            <p className='subtitle'>Search deals on hotels, homes, and much more...</p>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../../components/navbar/Navbar';
 import '../../sass/pages/_home.scss';
 import Header from '../../components/header/Header';
+import Hero from '../../components/hero/Hero';
 
 const Home = () => {
   return (
     <div className='home'>
       <Navbar/>
       <Header/>
+      <Hero/>
     </div>
   )
 }
